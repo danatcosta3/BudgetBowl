702640019737af514a7826a250cf62f01c8bb1fe
shreykumar <156003188+shreykumar12@users.noreply.github.com>
2025-10-25 17:04:55 -0400

    Update MainHeader.jsx

--- budgetbowl/src/components/MainHeader.jsx
+++ budgetbowl/src/components/MainHeader.jsx
@@ -5,7 +5,7 @@ function MainHeader() {
   return (
    <div className='bg-blue-300, w-full, h-10'>
     <Link to="/">
-    <p className='text-black'>Budget<p className='text-prim-main-dark'>Bowl</p></p>
+    <p className='text-black w-full'>Budget<p className='text-prim-main-dark' w-full>Bowl</p></p>
     </Link>
    </div>
   )
