d416f7a21ad28410ed580236be049952b5424277
shreykumar <156003188+shreykumar12@users.noreply.github.com>
2025-10-25 16:42:26 -0400

    Added colors to tailwind config

---
+++ budgetbowl/src/components/MainHeader.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function MainHeader() {
+  return (
+    <header style={{
+        backgroundColor: 'D9D9D9',
+        padding: '2rem'
+        
+    }}> 
+
+    </header>
+  )
+}
+
+export default MainHeader
--- budgetbowl/tailwind.config.js
+++ budgetbowl/tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx,}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "caribbean-current": "#08605fff",
+        "teal": "#177e89ff",
+        "hookers-green": "#598381ff",
+        "moss-green": "#8e936dff",
+        "olivine": "#a2ad59ff",
+      },
+    },
   },
   plugins: [],
 }
