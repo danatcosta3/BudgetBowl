7a8fb9c1e5cb04b7e1e3be1473303373d448d18d
Dana Costa <danatcosta3@yahoo.com>
2025-10-25 18:17:41 -0400

    Update Homepage

--- budgetbowl/src/components/MainHeader.jsx
+++ budgetbowl/src/components/MainHeader.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function MainHeader() {
   return (
-   <div className='bg-blue-600 w-full h-auto'>
-    <Link to="/">
-    <p className='text-black w-full flex flex-row text-4xl p-3'>Budget
-    <p className='text-prim-main-dark '>Bowl</p></p>
-    </Link>
-    <hr/>
-   </div>
-  )
+    <div className=" w-full h-auto">
+      <Link to="/">
+        <p className="text-black w-full flex flex-row text-4xl p-3">
+          Budget
+          <p className="text-prim-main-dark ">Bowl</p>
+        </p>
+      </Link>
+      <hr />
+    </div>
+  );
 }
 
-export default MainHeader
+export default MainHeader;
--- budgetbowl/src/pages/HomePage.jsx
+++ budgetbowl/src/pages/HomePage.jsx
@@ -3,12 +3,27 @@ import MainHeader from "../components/MainHeader.jsx";
 
 function HomePage() {
   return (
-    <div className="flex flex-row bg-white">
+    <div className="flex flex-col">
       <MainHeader />
-      {/* Left Side */}
-      <div></div>
-      {/* Picture Side */}
-      <div></div>
+      {/* Main Panel */}
+      <div className="flex h-screen">
+        {/* Left Side */}
+        <div className="w-2/3 h-full flex items-center justify-center">
+          <div className="w-2/3">
+            <h1 className="text-6xl mb-36">
+              <span className="text-black font-bold">Share.</span>{" "}
+              <span className="text-prim-main-sand font-bold">Eat.</span>{" "}
+              <span className="text-prim-main-olive font-bold">Repeat.</span>
+              <br />
+              Find Quick And Easy Meals While Minimizing Overhead Effort.
+            </h1>
+          </div>
+        </div>
+        {/* Picture Side */}
+        <div className="w-1/3 h-full bg-prim-main-blue flex items-center justify-center">
+          _
+        </div>
+      </div>
     </div>
   );
 }
