66e16f522dbc705c35aaf3995e13e659affd53ad
shreykumar <156003188+shreykumar12@users.noreply.github.com>
2025-10-25 19:46:46 -0400

    Update MainHeader.jsx

--- budgetbowl/src/components/MainHeader.jsx
+++ budgetbowl/src/components/MainHeader.jsx
@@ -14,7 +14,7 @@ function MainHeader() {
         <div className="flex gap-4 ml-auto">
           <Link to="/login">
             <button className="text-prim-main-dark px-6 py-2 rounded-lg font-semibold hover:bg-gray-100 transition">
-              Login
+              Log In
             </button>
           </Link>
 
