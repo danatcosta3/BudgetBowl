cfbc15204d4246dc891ba4fe8c4493350eab88e1
shreykumar <156003188+shreykumar12@users.noreply.github.com>
2025-10-25 21:42:06 -0400

    Added card and demo data

---
+++ budgetbowl/src/components/NibbleCards.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Clock } from "lucide-react";
+
+function NibbleCards({ name, ingredients, time, author, steps, image }) {
+  return (
+    <div className="border-4 border-prim-main-dark w-80 h-180 rounded-md shadow-lg overflow-hidden relative">
+      <div className="relative">
+        <img className="w-80 h-120 object-cover" src={image} alt="food" />
+        <div className="absolute top-2 right-2 bg-white/90 backdrop-blur-sm px-2 py-1 rounded-full flex items-center gap-1 shadow">
+          <Clock className="w-4 h-4 text-prim-main-blue" />
+          <span className="font-mono text-xs text-prim-main-blue">
+            {time} mins
+          </span>
+        </div>
+      </div>
+      <div className="pt-10 font-mono text-prim-main-blue">
+        <h1 className="text-2xl">{name}</h1>
+        <h2 className="pt-5 text-lg">By {author}</h2>
+        <h3 className="pt-5 text-lg">{ingredients.length} ingredients</h3>
+      </div>
+    </div>
+  );
+}
+
+export default NibbleCards;
--- budgetbowl/src/pages/GroceryListPage.jsx
+++ budgetbowl/src/pages/GroceryListPage.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import NibbleCards from "../components/NibbleCards";
 
 function GroceryListPage() {
-  return <div>GroceryListPage</div>;
+  const users = [
+    {
+      name: "chicken tikka",
+      author: "Shrey",
+      time: 15,
+      ingredients: ["chicken", "yogurt", "spices"],
+      steps: ["marinate chicken", "grill chicken", "serve"],
+      image: "chicken-tikka-masala.jpg",
+    },
+  ];
+  return (
+    <div className="grid pl-40 grid-cols-1 grid-rows-3 gap-6">
+      {users.map((user, index) => (
+        <NibbleCards key={index} {...user} />
+      ))}
+    </div>
+  );
 }
 
 export default GroceryListPage;
