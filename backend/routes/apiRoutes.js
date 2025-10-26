e303687868573e4cbc3baf4225c1cae4f9265630
Noah Joseph <noahjoseph0405@gmail.com>
2025-10-26 03:51:55 -0400

    Adding amazon s3

--- backend/routes/apiRoutes.js
+++ backend/routes/apiRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { registerUser } from "../controllers/userAuth.js";
+import { registerUser, login } from "../controllers/userAuth";
+import { uploadToS3 } from "../controllers/amazonAuth.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/login", login);
+router.post("/upload", uploadToS3);
 
 export default router;
