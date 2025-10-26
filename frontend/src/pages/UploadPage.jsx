1678d97b183a0235ccd0476460f65fd21286c4b1
Noah Joseph <noahjoseph0405@gmail.com>
2025-10-25 23:07:51 -0400

    upload innit

--- frontend/src/pages/UploadPage.jsx
+++ frontend/src/pages/UploadPage.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { use } from "react";
+import MainHeader from "../components/MainHeader";
+import { useState } from "react";
 
 function UploadPage() {
-  return <div>UploadPage</div>;
+  const [mealName, setMealName] = useState("");
+  const [etaMinutes, setEtaMinutes] = useState("15");
+  const [images, setImages] = useState("");
+  const Max_Images = 3;
+  const [ingredients, setIngredients] = useState([""]);
+  const [steps, setSteps] = useState([""]);
+  const [touched, setTouched] = useState(false);
+
+ 
+  return (
+    <div className="min-h-screen bg-white flex flex-col"> 
+      <header className="border-b border-gray 200 px-6 py-4 flex items-center justify-betw"></header>
+    </div>
+  );
 }
 
 export default UploadPage;
