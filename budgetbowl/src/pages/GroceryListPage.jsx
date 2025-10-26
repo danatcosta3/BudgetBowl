4c4c315e251b8e6e973e31ab8cf3d987c4ed4128
shreykumar <156003188+shreykumar12@users.noreply.github.com>
2025-10-25 21:50:44 -0400

    moved demo to likes page

--- budgetbowl/src/pages/GroceryListPage.jsx
+++ budgetbowl/src/pages/GroceryListPage.jsx
@@ -1,24 +1,6 @@
 import React from "react";
-import NibbleCards from "../components/NibbleCards";
 
 function GroceryListPage() {
-  const users = [
-    {
-      name: "chicken tikka",
-      author: "Shrey",
-      time: 15,
-      ingredients: ["chicken", "yogurt", "spices"],
-      steps: ["marinate chicken", "grill chicken", "serve"],
-      image: "chicken-tikka-masala.jpg",
-    },
-  ];
-  return (
-    <div className="grid pl-40 grid-cols-1 grid-rows-3 gap-6">
-      {users.map((user, index) => (
-        <NibbleCards key={index} {...user} />
-      ))}
-    </div>
-  );
+  return <div>GroceryListPage</div>;
 }
-
 export default GroceryListPage;
